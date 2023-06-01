57e617b37bc5d71a7a7f6d9bf77074be255100ef
luke-vg <vangenechtenluke@gmail.com>
2023-06-01 15:57:55 +0200

    Demo
    Merge conflict solved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faTrash, faPlus, faPen, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const initialGlobalState = {
-  count: 0,
   todos: []
 };
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#00008b', 
+    },
+    secondary: {main:'#ffffff'},
+  },
+});
 
 // Create a Context for the (global) State
 const GlobalState = React.createContext();
@@ -17,7 +28,9 @@ class Global extends React.Component {
     
 	// Set the initial (global) State
     this.state = {
+      showIcon: false,
       globals: initialGlobalState || {},
+      
     };
     
   }
@@ -31,17 +44,19 @@ class Global extends React.Component {
   setGlobalState = (data = {}) => {
     const { globals } = this.state;
     
+
     // Loop over the data items by key, only updating those which have changed
     Object.keys(data).forEach((key) => {
       globals[key] = data[key];
     });
 
     // Update the state with the new State
-    this.setState(globals);
+    this.setState({globals});
   };
 
   addTodo = (todo) => {
     this.setState(prevState => ({
+
       globals: {
         ...prevState.globals,
         todos: [...prevState.globals.todos, todo]
@@ -49,24 +64,29 @@ class Global extends React.Component {
     }));
   }
 
-toggleComplete = (index) => {
-  this.setState(prevState => {
-    const newTodos = prevState.globals.todos.map((todo,i) => {
-      if (i===index) {
-        return {...todo, completed: !todo.completed};
-      } else {
-        return todo;
-      }
-    });
-    return {
+  toggleComplete = (index) => {
+    this.setState((prevState) => ({
       globals: {
         ...prevState.globals,
-        todos: newTodos
-      }
-    }
-  }, () => console.log(this.state));
-}
-
+        todos: prevState.globals.todos.map((todo, i) =>
+          i === index
+            ? { ...todo, completed: !todo.completed, showIcon: !todo.showIcon }
+            : todo
+        ),
+      },
+    }));
+  };
+  
+  editTodo = (index, newText) => {
+    this.setState((prevState) => ({
+      globals: {
+        ...prevState.globals,
+        todos: prevState.globals.todos.map((todo, i) =>
+          i === index ? { ...todo, text: newText } : todo
+        ),
+      },
+    }));
+  };
   removeTodo = (index) => {
     this.setState(prevState => ({
       globals: {
@@ -78,29 +98,67 @@ toggleComplete = (index) => {
   
   render() {
     const { globals } = this.state;
-    const { Root } = this.props;
     
     return (
-      // Pass the current value of GlobalState, based on this components' State, down
-      <GlobalState.Provider value={globals}>
-        <InputField addTodo={this.addTodo}></InputField>
-        <TodoList todos={globals.todos} toggleComplete={this.toggleComplete} removeTodo={this.removeTodo} />
-         <h1>Todo app</h1>
-        <ul>
+      <ThemeProvider theme={theme}>
+<GlobalState.Provider
+      value={{
+        ...globals,
+        showIcon: this.state.showIcon,
+        addTodo: this.addTodo,
+        toggleComplete: this.toggleComplete,
+        removeTodo: this.removeTodo,
+        editTodo: this.editTodo,
+      }}
+    >   
+    <Router>
+      <div className="bg">
+        <div className="left-column">
 
-        </ul> 
-        <Root />
-        
+            <ul>
+              <li className="pagebtn-container"><a className="page-btn" href="/page1">Page 1</a></li>
+              <li className="pagebtn-container"><a className="page-btn" href="/page2">Page 2</a></li>
+              <li className="pagebtn-container"><a className="page-btn" href="/page3">Page 3</a></li>
+            </ul>
+        </div>
+        <div className="right-column">
+          <Routes>
+            <Route path="/page1" element={<Page1 globals={globals} />} />
+            <Route path="/page2" element={<Page2 />} />
+            <Route path="/page3" element={<Page3 />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
       </GlobalState.Provider>
+      </ThemeProvider>
     );
   }
 }
 
-// Create a shorthand Hook for using the GlobalState
-const useGlobalState = () => React.useContext(GlobalState);
+function Page1(props) {
+  const { globals } = props;
+  return (
+    <React.Fragment>
+      <InputField addTodo={globals.addTodo} />
+      <TodoList
+        showIcon={globals.showIcon}
+        todos={globals.todos}
+        toggleComplete={globals.toggleComplete}
+        removeTodo={globals.removeTodo}
+        editTodo={globals.editTodo}
+      />
+    </React.Fragment>
+  );
+}
 
-// Create an example component which both renders and modifies the GlobalState
+function Page2() {
+  return <h1>Page 2 Content</h1>;
+}
 
+function Page3() {
+  return <h1>Page 3 Content</h1>;
+}
 function UseInputValue(initialValue) {
    const [value, setValue] = useState(initialValue);
 
@@ -110,48 +168,136 @@ function UseInputValue(initialValue) {
   };
 }
 
-function TodoList({todos, toggleComplete, removeTodo}) {
+function TodoList() {
+  const [editIndex, setEditIndex] = useState(null);
+const [editText, setEditText] = useState('');
+  const { todos, toggleComplete, removeTodo, editTodo } = useContext(GlobalState);
   return (
     <ul>
-      {todos.map((todo,index) => (
-        <li key={index} style={{textDecoration: todo.completed ? 'line-through' : 'none'}}>
-          <FontAwesomeIcon icon={faCheck} onClick={() => toggleComplete(index)}></FontAwesomeIcon>{todo.text}
-          <button className="rm-button" onClick={() => removeTodo(index)}>
-            <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
-            </button>
-          </li>
-      ))}
-    </ul>
+  {todos.map((todo, index) => (
+    <li
+      key={index}
+      style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+    >
+      
+      <div className="container">
+        <div className="inputbtn">
+        <button className="icon-btn" onClick={() => toggleComplete(index)}>
+  {todo.showIcon && <FontAwesomeIcon icon={faCheck} />}
+</button>
+{editIndex === index ? (
+  <>
+    <input
+      type="text"
+      value={editText}
+      onChange={(e) => setEditText(e.target.value)}
+    />
+    <Button
+  variant="contained"
+  color="primary"
+  type="submit"
+  sx={{
+    margin: '15px',
+    minWidth: 'unset',
+    borderRadius: '25%',
+    padding: 0,
+    width: '48px',
+    height: '48px',
+    minHeight: 'unset',
+  }}
+
+      onClick={() => {
+        editTodo(editIndex, editText);
+        setEditIndex(null);
+        setEditText('');
+      }}
+    >
+      <FontAwesomeIcon icon={faFloppyDisk} />
+    </Button>
+  </>
+) : (
+  todo.text
+)}
+      
+        </div>
+        <Button
+  variant="contained"
+  color="primary"
+  type="submit"
+  sx={{
+    margin: '15px',
+    minWidth: 'unset',
+    borderRadius: '25%',
+    padding: 0,
+    width: '48px',
+    height: '48px',
+    minHeight: 'unset',
+  }} onClick={() => removeTodo(index)}>
+          <FontAwesomeIcon icon={faTrash} />
+        </Button>
+        <Button
+  variant="contained"
+  color="primary"
+  type="submit"
+  sx={{
+    margin: '15px',
+    minWidth: 'unset',
+    borderRadius: '25%',
+    padding: 0,
+    width: '48px',
+    height: '48px',
+    minHeight: 'unset',
+  }}
+  onClick={() => {
+    setEditIndex(index);
+    setEditText(todo.text);
+  }}
+>
+        <FontAwesomeIcon icon={faPen} />
+      </Button>
+      </div>
+    </li>
+  ))}
+</ul>
   )
 }
 
-function InputField({ addTodo }) {
+function InputField() {
+  const { addTodo } = useContext(GlobalState);
   const name = UseInputValue('');
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newTodo = { text: name.value};
+    const newTodo = { text: name.value, showIcon: false };
     addTodo(newTodo);
-    name.onChange({ target: { value: ''}});
-  }
+    name.onChange({ target: { value: '' } });
+  };
   return (
     <form onSubmit={handleSubmit}>
-    <input type="text" {...name} />
-    <button type="submit">Add</button>
+      <input className="fieldStyle" type="text" {...name} />
+      <Button
+  variant="contained"
+  color="primary"
+  type="submit"
+  sx={{
+    margin: '15px',
+    minWidth: 'unset',
+    borderRadius: '25%',
+    padding: 0,
+    width: '48px',
+    height: '48px',
+    minHeight: 'unset',
+  }}>
+        <FontAwesomeIcon icon={faPlus} />
+      </Button>
     </form>
-  )
+  );
 }
 
 function SomeComponent() {
-  const { count } = useGlobalState();
+  //const { count } = useGlobalState();
 
-  // Create a function which mutates GlobalState
-  function incrementCount() {
-    GlobalState.set({
-      count: count + 1,
-    });
-  }
-
-  return <div onClick={incrementCount}>{count}</div>;
+  
 }
 
 export default function poc() {
